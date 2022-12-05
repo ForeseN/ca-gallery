84aa785fa4700a8480ad7ab138b0be9ab6e4a9e4
ForeseN <yagosik@gmail.com>
2022-12-05 13:40:38 +0200

    init

---
+++ js/main.js
@@ -0,0 +1,8 @@
+'use strict'
+
+// A $( document ).ready() block.
+$(document).ready(initPage())
+
+function initPage() {
+    console.log('yES')
+}
---
+++ js/proj-service.js
@@ -0,0 +1,59 @@
+'use strict'
+
+const projects = [
+    {
+        id: 'chess',
+        name: 'Chess',
+        title: 'Better push those boxes',
+        desc: 'lorem ipsum lorem ipsum lorem ipsum',
+        url: 'projs/chess',
+        publishedAt: 1448693940000,
+        labels: ['Matrixes', 'keyboard events'],
+    },
+    {
+        id: 'minesweeper',
+        name: 'Minesweeper',
+        title: 'Better push those boxes',
+        desc: 'lorem ipsum lorem ipsum lorem ipsum',
+        url: 'projs/minesweeper',
+        publishedAt: 1448693940000,
+        labels: ['Matrixes', 'keyboard events'],
+    },
+    {
+        id: 'pokerhouse',
+        name: 'PokerHouse',
+        title: 'Better push those boxes',
+        desc: 'lorem ipsum lorem ipsum lorem ipsum',
+        url: 'projs/pokerhouse',
+        publishedAt: 1448693940000,
+        labels: ['Matrixes', 'keyboard events'],
+    },
+    // same
+    {
+        id: 'chess',
+        name: 'Chess',
+        title: 'Better push those boxes',
+        desc: 'lorem ipsum lorem ipsum lorem ipsum',
+        url: 'projs/chess',
+        publishedAt: 1448693940000,
+        labels: ['Matrixes', 'keyboard events'],
+    },
+    {
+        id: 'minesweeper',
+        name: 'Minesweeper',
+        title: 'Better push those boxes',
+        desc: 'lorem ipsum lorem ipsum lorem ipsum',
+        url: 'projs/minesweeper',
+        publishedAt: 1448693940000,
+        labels: ['Matrixes', 'keyboard events'],
+    },
+    {
+        id: 'pokerhouse',
+        name: 'PokerHouse',
+        title: 'Better push those boxes',
+        desc: 'lorem ipsum lorem ipsum lorem ipsum',
+        url: 'projs/pokerhouse',
+        publishedAt: 1448693940000,
+        labels: ['Matrixes', 'keyboard events'],
+    },
+]
